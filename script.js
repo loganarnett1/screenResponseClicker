f3bd92fdd5e4fc4f8a7cb8003818d7d2233bf3df
loganarnett1 <loganarnett@gmail.com>
2022-04-09 23:46:14 -0400

    Created Page

---
+++ script.js
@@ -0,0 +1,35 @@
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+
+let winnningSide = -1;
+button1.addEventListener("click", function()
+{
+    if(winnningSide === -1)
+    {
+        winnningSide = 0;
+    }
+    setWinningSide();
+});
+
+button2.addEventListener("click", function()
+{
+    if(winnningSide === -1)
+    {
+        winnningSide = 1;
+    }
+    setWinningSide();
+});
+
+function setWinningSide()
+{
+    if(winnningSide === 0)
+    {
+        button1.classList.add("winningSide");
+        button2.classList.remove("winningSide");
+    }
+    else if(winnningSide === 1)
+    {
+        button2.classList.add("winningSide");
+        button1.classList.remove("winningSide");
+    }
+}
