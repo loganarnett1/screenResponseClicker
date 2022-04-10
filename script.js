4803ba4cef6d379ad44291b5fb2b37f5be7c24fd
loganarnett1 <loganarnett@gmail.com>
2022-04-10 00:03:27 -0400

    onTouch Change

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 
 let winnningSide = -1;
-button1.addEventListener("touchStart", function()
+button1.ontouchstart = button1Touch;
+button2.ontouchstart = button2Touch;
+
+/*
+button1.addEventListener("touchStart", function(e)
 {
+    e.preventDefault();
     if(winnningSide === -1)
     {
         winnningSide = 0;
@@ -11,14 +16,36 @@ button1.addEventListener("touchStart", function()
     setWinningSide();
 });
 
-button2.addEventListener("touchStart", function()
+button2.addEventListener("touchStart", function(e)
 {
+    e.preventDefault();
     if(winnningSide === -1)
     {
         winnningSide = 1;
     }
     setWinningSide();
 });
+*/
+function button1Touch(e)
+{
+    e.preventDefault();
+    if(winnningSide === -1)
+    {
+        winnningSide = 0;
+    }
+    setWinningSide();
+}
+
+function button2Touch(e)
+{
+    e.preventDefault();
+    if(winnningSide === -1)
+    {
+        winnningSide = 1;
+    }
+    setWinningSide();
+}
+
 
 function setWinningSide()
 {
